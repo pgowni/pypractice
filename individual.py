e2ce6062dd934843c7f4a93964e89968dcd9a611
pgowni <65507706+pgowni@users.noreply.github.com>
2024-09-10 13:13:47 +0530

    Update individual.py

--- individual.py
+++ individual.py
@@ -4,9 +4,18 @@ class IndividualCustomer(Customer):
     def to_dict(self):
         return super().to_dict()
     
+    @staticmethod
+    def to_obj(data):
+        return Customer.to_obj(data)
+    
 
 class IndividualPolicy(Policy):
     def to_dict(self):
         return super().to_dict()
+    
+    @staticmethod
+    def to_obj(data):
+       return Policy.to_obj(data)
+    
 
 
