9acae00483cf3bc034f9ec74f327ed92197095c4
pgowni <65507706+pgowni@users.noreply.github.com>
2024-09-10 13:13:19 +0530

    Update models.py

--- models.py
+++ models.py
@@ -1,3 +1,5 @@
+import json
+
 class Customer:
     def __init__(self, id, type, first_name, last_name, email, dob, policies):
         self.id = id
@@ -7,6 +9,18 @@ class Customer:
         self.email = email
         self.dob = dob
         self.policies = policies
+    
+    @staticmethod
+    def to_obj(data):
+        return Customer(
+            id=data['CustomerID'],
+            type=data['CustomerType'],
+            first_name=data['FirstName'],
+            last_name=data['LastName'],
+            email=data['Email'],
+            dob=data['DateOfBirth'],
+            policies = [policy['policyID'] for policy in data["policies"]]
+        )
 
     def to_dict(self):
         return {
@@ -20,8 +34,21 @@ class Customer:
             "policies": self.policies
         }
     
+    def total_premium(self):
+        total = 0
+        with open("JSON\policies.json", 'r') as input_file:
+            curr_customer_policies = json.load(input_file) 
+        for policy in curr_customer_policies:
+            if policy["PolicyID"] in set(self.policies):
+                total += policy["Premium"]
+        
+        return total
+
 
 class Policy:
+
+    curr_policy_id = 'POL12356'
+
     def __init__(self, id, type, start_date, end_date, premium):
         self.id = id
         self.type = type
@@ -37,20 +64,22 @@ class Policy:
             "EndDate": self.end_date,
             "Premium": self.premium
         }
+    
+    @staticmethod
+    def to_obj(data):
+        return Policy(
+            id=data['PolicyID'],
+            type=data['PolicyType'],
+            start_date=data['StartDate'],
+            end_date=data['EndDate'],
+            premium=data['Premium'],
+        )
+    @classmethod
+    def generate_policy_id(cls):
+        id_serial = int(cls.curr_policy_id[3:])
+        id_serial += 1
+        cls.curr_policy_id = cls.curr_policy_id[:3] + str(id_serial)
+        return cls.curr_policy_id
 
-    # @staticmethod
-    # def generate_policy_id(policies):
-    #     dict_policies = []
-
-    #     for obj in policies:
-    #         dict_policies.append(obj.to_dict())
-        
-    #     if not dict_policies:
-    #         return "POL10001"  
-        
-    #     last_policy_id = 0
-    #     for policy in dict_policies:
-    #         last_policy_id = max(last_policy_id, int(policy["PolicyID"][3:]))
-    #     new_policy_id = f"POL{last_policy_id + 1}"
-    #     return new_policy_id
+    
 
